d356e58760efaa2d0ee2279506e85efff9db2c73
ishida.m <ishida.m@opst.co.jp>
2016-06-15 10:34:14 +0900

    投稿しようとするとsocket接続エラーがでてますがとりあえずhtmlは表示されるようになりました

--- javajoSample/src/main/java/jp/javajo/controller/TestController.java
+++ javajoSample/src/main/java/jp/javajo/controller/TestController.java
@@ -4,19 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 接続テストクラス
  * Created by maaya
  */
 @Controller
+@RequestMapping("/")
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     /**
      * 疎通確認
      */
-    @RequestMapping(value = "/")
+    @RequestMapping(method = RequestMethod.GET)
     String test() {
         logger.debug("testメソッド");
         return "hello";
